c69fe09a3fc1133aa4a213ffe568cb41b2c32c57
Ahnjisoo <sfau2014@gmail.com>
2025-12-28 14:54:51 +0900

    1. kafka 4.0 교체로 인한 zookeeper 의존성 제거

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -1,11 +1,7 @@
-sudo kubectl apply -f kafka-headless.yaml -f kafka-statefulset.yaml -f kafka-nodeport.yaml -n dev
-sudo kubectl -n dev port-forward svc/kafka-nodeport 19094:19094 > /dev/null 2>&1 &
+uuid=$(uuidgen) yq -i '.data.KAFKA_CLUSTER_ID=env(uuid)' kafka-configmap.yaml
+sudo kubectl apply -f kafka-configmap.yaml -n dev
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 replication-factor 1 partition 1 --topic topic-test
+kubectl rollout status statefulset/kafka-br -n dev --timeout=600s
+kubectl apply -f kafka-job.yaml -n dev
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --delete --bootstrap-server localhost:9092 -topic topic-test
-
-/opt/bitnami/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
-
-/opt/bitnami/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic --from-beginning
-sudo kubectl -n dev exec -it kafka-0 -- bash
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
