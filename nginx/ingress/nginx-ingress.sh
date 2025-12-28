a8c0b0c8d6a6cc825ffbef96346d233ec94c5280
Ahnjisoo <sfau2014@gmail.com>
2025-12-28 15:03:09 +0900

    1. knox용 ingress 설정

--- nginx/ingress/nginx-ingress.sh
+++ nginx/ingress/nginx-ingress.sh
@@ -2,3 +2,4 @@ sudo kubectl apply -f argocd-ingress.yaml -n argocd
 sudo kubectl apply -f gitlab-ingress.yaml -n dev
 sudo kubectl apply -f admin-nginx-ingress.yaml -n dev
 sudo kubectl apply -f kafka-ui-ingress.yaml -n dev
+sudo kubectl apply -f knox-ingress.yaml -n dev
